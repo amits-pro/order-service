31160636cce439ca62f4049be96e7e54d0af4e01
amit <amits.brg@gmail.com>
2019-10-17 15:29:01 +0530

    Order service - version 1

--- src/main/java/com/wakanda/orderservice/OrderServiceApplication.java
+++ src/main/java/com/wakanda/orderservice/OrderServiceApplication.java
@@ -8,7 +8,7 @@ public class OrderServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
-		System.out.println("this is order service");
+		System.out.println("this is order service - version 1");
 	}
 
 }
